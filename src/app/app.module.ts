0cdee0ead00dbde001a40c486707f9b8586d01c4
Santiago Urbano <m.urbanod@uniandes.edu.co>
2023-09-05 20:39:06 -0500

    Parte del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CultureDetailsComponent } from './components/culture-details/culture-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
+import { RestaurantesComponent } from './components/restaurantes/restaurantes.component';
+import { CulturasService } from './services/culturas.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CultureDetailsComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    RecetasComponent,
+    RestaurantesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CulturasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
